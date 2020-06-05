8b5aa1913e54f548064851b676883c72c5a81acd
Yuval Langer <yuvallangerontheroad@gmail.com>
2020-06-05 19:52:12 +0300

    Add Hilbert curve curve.

--- dabbling/console.py
+++ dabbling/console.py
@@ -2,6 +2,7 @@ import click
 
 from . import __version__
 from .dragon_curve import dragon_curve, path_to_curvy_svg_no_clue, path_to_svg_file_content, write_matplotlib_png
+from .hilbert_curve import hilbert_curve, make_svg_content
 
 
 @click.command()
@@ -14,7 +15,7 @@ from .dragon_curve import dragon_curve, path_to_curvy_svg_no_clue, path_to_svg_f
         show_default=True,
 )
 @click.version_option(version=__version__)
-def main(iterations: str):
+def main_dragon(iterations: str):
     l = dragon_curve(int(iterations))
 
     svg_content = path_to_svg_file_content(l)
@@ -28,3 +29,21 @@ def main(iterations: str):
     write_matplotlib_png(l)
 
     click.echo(l)
+
+
+@click.command()
+@click.option(
+        '--iterations',
+        '-n',
+        default='5',
+        help='Number of iterations',
+        metavar='ITERS',
+        show_default=True,
+)
+@click.version_option(version=__version__)
+def main_hilbert(iterations: str):
+    l = hilbert_curve(int(iterations))
+
+    svg_content = make_svg_content(l)
+    with open('hilbert-curve.svg', 'w') as f:
+        f.write(svg_content)
