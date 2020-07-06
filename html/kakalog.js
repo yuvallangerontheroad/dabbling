38a4ae74c13cb23bb1ce8b4f7c9f8bc8f89e3a55
Yuval Langer <yuvallangerontheroad@gmail.com>
2020-07-06 15:07:20 +0300

    Add no console on firefox android workaround.

---
+++ html/kakalog.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+console.log('kakalog.js start.');
+
+
+(function() {
+	function change_console() {
+		if (window.kakalog_activated) {
+			[
+				'log',
+				'error',
+				'info',
+				'trace',
+			].forEach(function_name=>{
+				let old_function = console[function_name];
+				let new_function = function(...stuff) {
+					old_function(...stuff);
+					let log_element = document.getElementById('log');
+					let log_text = `${function_name}: ${stuff}`;
+					log_element.innerText = `${log_element.innerText}\n${log_text}`;
+				};
+				console[function_name] = new_function;
+			});
+		};
+	};
+	
+	window.addEventListener('load', change_console);
+})();
+
+
+console.log('kakalog.js end.');
